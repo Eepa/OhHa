2232c3cb80c73bef0a9740ca96e8bc1dd05593e2
Eveliina Pakarinen <pevetiainen.ep@hotmail.com>
2013-01-11 17:11:34 +0200

    Harjoitusprojekti lisatty ja .gitignore

---
+++ OhHaOhjelma/src/ohhaohjelma/OhHaOhjelma.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohhaohjelma;
+
+/**
+ *
+ * @author Eveliina Pakarinen
+ */
+public class OhHaOhjelma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
