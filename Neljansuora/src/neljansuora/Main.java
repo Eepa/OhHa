d6d7e31c25adc7a59b029e13dd6265b43c3776c0
Eveliina Pakarinen <pevetiainen.ep@hotmail.com>
2013-01-24 16:55:46 +0200

    kayttoliittyman aloitus

--- Neljansuora/src/neljansuora/Main.java
+++ Neljansuora/src/neljansuora/Main.java
@@ -2,6 +2,8 @@
 package neljansuora;
 
 import java.util.Scanner;
+import javax.swing.SwingUtilities;
+import neljansuora.kayttoliittyma.Kayttoliittyma;
 import neljansuora.peli.Neljansuora;
 
 
@@ -12,12 +14,15 @@ public class Main {
   
     public static void main(String[] args) {
         // TODO code application logic here
+        Kayttoliittyma kayttoliittyma = new Kayttoliittyma();
+        SwingUtilities.invokeLater(kayttoliittyma);
         
         Scanner lukija = new Scanner(System.in);
         
         Neljansuora neljansuora = new Neljansuora(7, 6, 4, lukija);
         
         neljansuora.kaynnista();
+        
         
    
     }
--- Neljansuora/src/neljansuora/kayttoliittyma/Kayttoliittyma.java
+++ Neljansuora/src/neljansuora/kayttoliittyma/Kayttoliittyma.java
@@ -1,7 +1,36 @@
 
 package neljansuora.kayttoliittyma;
 
+import java.awt.Container;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
-public class Kayttoliittyma {
+
+public class Kayttoliittyma implements Runnable {
+    
+    private JFrame frame;
+
+    @Override
+    public void run() {
+        frame = new JFrame("Neljän Suora");
+        frame.setPreferredSize(new Dimension(500, 500));
+        
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
+        luoKomponentit(frame.getContentPane());
+        
+        frame.pack();
+        frame.setVisible(true);
+        
+    }
+    
+    private void luoKomponentit(Container container){
+        
+    }
+    
+    public JFrame getFrame(){
+        return this.frame;
+    }
     
 }
